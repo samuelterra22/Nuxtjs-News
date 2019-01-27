6eda34eea2abaed8cfba95baa2fa76a371929554
Samuel Terra <samuelterra22@hotmail.com>
2019-01-26 23:39:43 -0200

    Fix lint

--- components/Card.vue
+++ components/Card.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="w-full md:w-1/2 lg:w-1/3 flex flex-col mb-8 px-3">
     <div class="overflow-hidden bg-white rounded-lg shadow hover:shadow-raised hover:translateY-2px transition">
-      <a :href="article.url" target="_blank">
+      <a
+        :href="article.url"
+        target="_blank"
+      >
         <img
           class="w-full h-64"
           :src="article.urlToImage"
@@ -9,7 +12,10 @@
         >
       </a>
       <div class="p-6 flex flex-col justify-between ">
-        <h3 class="font-medium text-grey-darkest mb-4 leading-normal" :title="article.title ">
+        <h3
+          class="font-medium text-grey-darkest mb-4 leading-normal"
+          :title="article.title "
+        >
           {{ article.title | truncate(44) }}
         </h3>
         <p class="inline-flex items-center">
