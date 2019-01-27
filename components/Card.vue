4d735c57c2d05097a3af85832c890b66e9f00651
Samuel Terra <samuelterra22@hotmail.com>
2019-01-26 23:19:51 -0200

    Add link into image

--- components/Card.vue
+++ components/Card.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="w-full md:w-1/2 lg:w-1/3 flex flex-col mb-8 px-3">
     <div class="overflow-hidden bg-white rounded-lg shadow hover:shadow-raised hover:translateY-2px transition">
-      <img
-        class="w-full h-64"
-        :src="article.urlToImage"
-        alt="Sunset in the mountains"
-      >
+      <a :href="article.url" target="_blank">
+        <img
+          class="w-full h-64"
+          :src="article.urlToImage"
+          alt="Sunset in the mountains"
+        >
+      </a>
       <div class="p-6 flex flex-col justify-between ">
         <h3 class="font-medium text-grey-darkest mb-4 leading-normal" :title="article.title ">
           {{ article.title | truncate(44) }}
