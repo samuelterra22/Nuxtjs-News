64851547dcd45dbcf51c4d5ff15afb1e6f410df2
Samuel Terra <samuelterra22@hotmail.com>
2019-01-27 00:00:35 -0200

    Add loading component

---
+++ components/VLoading.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="w-full h-full">
+    <template v-if="isLoading">
+      <div class="w-full h-full flex items-center text-center flex-wrap text-white">
+        <div class="lds-facebook m-auto">
+          <div /><div /><div />
+        </div>
+      </div>
+    </template>
+    <template v-else>
+      <slot />
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VLoading',
+  props: {
+    isLoading: {
+      type: Boolean,
+      required: true
+    },
+    message: {
+      type: String,
+      default: 'Loading'
+    }
+  },
+  data: () => ({})
+}
+</script>
+
+<style scoped>
+</style>
